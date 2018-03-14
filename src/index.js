89a5752f9150473ca135aacf1db4aa6faca86c12
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-03-14 21:39:59 +0300

    Implement function longestConsecutiveLength using set as hash | 291ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let set = new Set();
+    let longestConsecutive = 0;
+    let n = array.length;
+    for (let i=0; i<n; ++i) {
+        set.add(array[i]);
+    }
+
+    for (let i=0; i<n; ++i)
+    {
+        if (!set.has(array[i]-1))
+        {
+            let j = array[i];
+            while (set.has(j))
+                j++;
+            if (longestConsecutive<j-array[i])
+                longestConsecutive = j-array[i];
+        }
+    }
+    return longestConsecutive;
 }
